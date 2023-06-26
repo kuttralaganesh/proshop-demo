62d1bb140950be8e46434fbae6a4cbf1f1f6eacb
kuttralaganesh <44425346+kuttralaganesh@users.noreply.github.com>
2023-06-26 12:57:27 +0530

    list out product

---
+++ frontend/src/components/Products.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Card } from 'react-bootstrap'
+
+const Products = () => {
+  return (
+    <Card className='my-3 p-3 rounded'>
+        
+    </Card>
+  )
+}
+  
+export default Products
