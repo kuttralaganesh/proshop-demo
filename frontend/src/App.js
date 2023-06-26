fc18a54b3a3d72ed626fea2ed86bbc3535ed9b7b
kuttralaganesh <44425346+kuttralaganesh@users.noreply.github.com>
2023-06-26 19:13:26 +0530

    List products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import Header  from "./components/Header";
 import Footer from "./components/Footer";
@@ -10,7 +11,7 @@ const App = () => {
       <Header/> 
         <main className="py-3">
           <Container>
-            <HomeScreen/>
+             <Outlet/>
           </Container>
         </main>
         <Footer/>
